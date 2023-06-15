b1f6b4d99a9f89a21fbf3456d69546bcaa066f66
rimskii <rimma.kubanova@gmail.com>
2023-06-15 19:48:58 +0600

    ffff

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import About from '@/components/About'
-import About from '@/components/Contact'
+import Contact from '@/components/Contact'
 import Image from 'next/image'
 
 export default function Home() {
